d73c72fea182cca792e3f889a72727bd6578a4b3
y-ken <y.ken.studio@gmail.com>
2013-07-20 22:09:32 +0900

    typo fixed

--- mrblib/fluent-logger.rb
+++ mrblib/fluent-logger.rb
@@ -1,24 +1,20 @@
 class Fluent
   DEFAULTADDRESS  = "127.0.0.1"
-  DEFAULTPORT     = 24224
+  DEFAULTPORT     = 8888
   
   class Logger
-    def initialize(tag_prefix, address, port)
-      adress = DEFAULTADDRESS if adress.nil?
-      port   = DEFAULTAPORT if adress.nil?
-      @url = 'http://#{address}:#{port}/#{tag_prefix}'
-      puts "Fluent::Logger initialized #{@url}"
+    def initialize(tag_prefix, address=nil, port=nil)
+      address = DEFAULTADDRESS if address.nil?
+      port   = DEFAULTPORT if port.nil?
+      @url = "http://#{address}:#{port}/#{tag_prefix}"
     end
 
     def post(tag, data)
-      puts "#{@url}#{tag}"
-      puts "post #{data}"
       http = HttpRequest.new()
       http.post("#{@url}#{tag}", {
         :json => JSON::stringify(data),
       },{
-        'User-Agent' => "mruby-fluent-logger",
-        "Content-Type" => 'application/x-www-form-urlencoded',
+        'Content-Type' => 'application/x-www-form-urlencoded',
       })
     end
   end
